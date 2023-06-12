ed43a3a2a309091cb3a4fe968fb01edaea51daeb
Janek <j.p.walaszek@gmail.com>
2023-06-12 13:35:45 +0200

    Add files

---
+++ script.js
@@ -0,0 +1,10 @@
+const DEFAULT_COLOR = '##242424';
+const DEFAULT_MODE = 'singleColor';
+const DEFAULT_SIZE = '16';
+
+const grid = document.getElementById('grid');
+
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
+
