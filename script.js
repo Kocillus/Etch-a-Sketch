486fb0cc20f269609e4541825986a5eae650627f
Kocilla <jan.walaszek@pm.me>
2023-06-14 17:50:54 +0200

    Fix buttons and rework clear action

--- script.js
+++ script.js
@@ -1,56 +1,62 @@
 click = true;
-let currentColor = "black";
-
+let currentColor = "#FCAE1E";
+let currentAction = "currentColor"
+ 
 function boardFullfilment(size) {
     let grid = document.querySelector('.gridBox')
     let squares = grid.querySelectorAll("div");
     squares.forEach((div) => div.remove());
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
+ 
     let amount = size * size;
     for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
-        square.addEventListener("mouseover", colorSquares);
+        square.addEventListener("mouseover", event => colorSquares(square));
         square.style.backgroundColor = "white";
         grid.insertAdjacentElement("beforeend", square);
     }
-
+ 
     document.getElementById("gridSizeOptionsOutput").innerHTML = size + ' x ' + size;
 }
-
+ 
 boardFullfilment(20);
-
+ 
 function changeSize(input) {
     boardFullfilment(input);
 }
-
-function colorSquares() {
+ 
+function colorSquares(square ) {
     if (click) {
-      if (color === "currentColor") {
-        this.style.backgroundColor = currentColor;
+      if (currentAction === "currentColor") {
+        square.style.backgroundColor = currentColor;
+        console.log("dupa");
       } 
-
-      if (color === "shadowing") {
-        this.style.backgroundColor = currentColor;
+ 
+      if (currentAction === "black") {
+        square.style.backgroundColor = "black";
       } 
-      
-      if (color === "rainbowMadness") {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+ 
+      if (currentAction === "rainbowMadness") {
+        square.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
       } 
-
-      if (color === "eraser") {
-        this.style.backgroundColor = "white";
+ 
+      if (currentAction === "eraser") {
+        square.style.backgroundColor = "white";
       } 
-
-      if (color === "clear") {
-        let grid = document.querySelector('.gridBox')
-        let squares = grid.querySelectorAll("div");
-        squares.forEach((div) => (div.style.backgroundColor = "white"));
-      }
     }
   }
+ 
+  function clearBoard() {
+    let grid = document.querySelector('.gridBox')
+    let squares = grid.querySelectorAll("div");
+    squares.forEach((div) => (div.style.backgroundColor = "white"));
+  }
 
-  function changeColor(choice) {
-    color = choice;
+  function changeAction(choice) {
+    currentAction = choice;
+  }
+ 
+  function changeColor(col) {
+    currentColor=col;
   }
